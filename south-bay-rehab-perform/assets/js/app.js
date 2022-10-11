bc3cad1100d045a24a0b7a3584e43696de78d14f
Jomel Bautista <jomelbautista@Jomels-MacBook-Pro.local>
2022-10-11 10:34:15 -0700

    showcase video edits

--- south-bay-rehab-perform/assets/js/app.js
+++ south-bay-rehab-perform/assets/js/app.js
@@ -6,13 +6,6 @@ window.onscroll = function() {
     }
 }
 
-function redirect() {
-    document.querySelector('.thanks').classList.remove('thanks-hidden');
-    setTimeout(() => {
-        window.location.href = 'https://southbayrandp.janeapp.com/#/staff_member/1/treatment/1';
-    }, 2000); // Wait 2 seconds and then redirect.
-}
-
 // Animate On Scroll
 AOS.init({
     duration: 750
